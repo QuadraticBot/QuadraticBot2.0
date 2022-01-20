1710cc95d6cf80659b5df18f7ba4c8193d4f7b27
Henry Hiles <henry@henryhiles.com>
2022-01-20 10:25:23 -0500

    Changed lets to consts in ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -5,15 +5,15 @@ module.exports = {
         .setName("ping")
         .setDescription("Replies with latency and uptime."),
     execute: async (interaction) => {
-        let message = `Pong!\nLatency: ${bold(interaction.client.ws.ping)}.`
+        const message = `Pong!\nLatency: ${bold(interaction.client.ws.ping)}.`
         let totalSeconds = interaction.client.uptime / 1000
-        let days = Math.floor(totalSeconds / 86400)
+        const days = Math.floor(totalSeconds / 86400)
         totalSeconds %= 86400
-        let hours = Math.floor(totalSeconds / 3600)
+        const hours = Math.floor(totalSeconds / 3600)
         totalSeconds %= 3600
-        let minutes = Math.floor(totalSeconds / 60)
-        let seconds = Math.floor(totalSeconds % 60)
-        let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`
+        const minutes = Math.floor(totalSeconds / 60)
+        const seconds = Math.floor(totalSeconds % 60)
+        const uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`
         message = message + `\nMy uptime is ${bold(uptime)}!`
         await interaction.reply({ content: message, ephemeral: true })
     },
